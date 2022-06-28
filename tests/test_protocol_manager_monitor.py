3cde6310991d164cdd87bace164417be071e5c5d
Rares Stanciu <rares.stanciu@outlook.com>
2022-06-28 15:50:01 +0300

    fix: tests

--- tests/test_protocol_manager_monitor.py
+++ tests/test_protocol_manager_monitor.py
@@ -11,20 +11,22 @@ class ProtocolManagerMonitorTests(TestCase):
     def test_no_error_on_empty_protocols_list(self) -> None:
         self.monitor.check_if_enough_balance([])
 
-    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_balance", return_value=100_000_000)
+    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_coverage_left", return_value=100_000_000)
     def test_no_error_on_one_protocol_with_enough_balance(self, *args) -> None:
         self.monitor.check_if_enough_balance([{"bytes_identifier": "0xdeadbeef"}])
 
-    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_balance", return_value=999)
+    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_coverage_left", return_value=999)
     def test_no_error_on_one_protocol_with_low_balance(self, *args) -> None:
         with self.assertRaises(expected_exception=MonitorException):
             self.monitor.check_if_enough_balance([{"bytes_identifier": "0xdeadbeef"}])
 
-    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_balance", return_value=100_000_000)
+    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_coverage_left", return_value=100_000_000)
     def test_no_error_on_multiple_protocols_with_enough_balance(self, *args) -> None:
         self.monitor.check_if_enough_balance([{"bytes_identifier": "0xdeadbeef"}, {"bytes_identifier": "0xdeadbeef"}])
 
-    @mock.patch("monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_balance", side_effect=[100_000_000, 999])
+    @mock.patch(
+        "monitors.protocol_manager.ProtocolManagerMonitor.get_protocol_coverage_left", side_effect=[100_000_000, 999]
+    )
     def test_no_error_on_multiple_protocols_and_one_with_low_balance(self, *args) -> None:
         with self.assertRaises(expected_exception=MonitorException):
             self.monitor.check_if_enough_balance(
